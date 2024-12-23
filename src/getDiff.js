a8181a8eeadb207d7800629476509b6cd0fb1948
Oleg Osipov <olegish@bk.ru>
2024-12-23 18:14:50 +0300

    add v2_getDiff.js

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,20 +1,26 @@
 import _ from 'lodash';
 
 const getDiff = (obj1, obj2) => {
-  const keys = new Set([...Object.keys(obj1), ...Object.keys(obj2)]);
-  const result = {};
-  keys.forEach((key) => {
-    if (!Object.hasOwn(obj1, key)) {
-      result[`+ ${key}: ${obj2[key]}`] = 'null'; // Добавлено в obj2
-    } else if (!Object.hasOwn(obj2, key)) {
-      result[`- ${key}: ${obj1[key]}`] = null; // Удалено из obj1
-    } else if (obj1[key] !== obj2[key]) {
-      result[`- ${key}: ${obj1[key]}`] = null; // Изменилось в obj1
-      result[`+ ${key}: ${obj2[key]}`] = null; // Изменилось в obj2
-    } else {
-      result[key] = obj1[key]; // Не изменилось
+  const keys1 = _.keys(obj1);
+  const keys2 = _.keys(obj2);
+  const sortedKeys = _.sortBy(_.union(keys1, keys2));
+
+  const result = sortedKeys.map((key) => {
+    if (!_.has(obj1, key)) {
+      return `+ ${key}: ${obj2[key]}`; // Добавлено в obj2
     }
+
+    if (!_.has(obj2, key)) {
+      return `- ${key}: ${obj1[key]}`; // Удалено из obj1
+    }
+
+    if (obj1[key] !== obj2[key]) {
+      return [`- ${key}: ${obj1[key]}`]; // Изменилось в obj1
+      // return [`+ ${key}: ${obj2[key]}`]; // Изменилось в obj2
+    }
+    return key; // Не изменилось
   });
+
   return result;
 };
 
